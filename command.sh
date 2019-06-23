430090791da0c74975dff01b938e47826c9778b5
Arthur Gonzaga <arthurbilul@gmail.com>
2019-06-23 15:26:10 -0300

    Add verification if what the user type is not null

--- command.sh
+++ command.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 function create-theme(){
 	PATH_FOLDER=~/Development/Desktop/Python/FlutterCustomColors
-    python $PATH_FOLDER/create-theme.py $1 $PWD
+    NUMBER_OF_COLORS=$1
+    if [ -n "$NUMBER_OF_COLORS" ]
+    then
+        python $PATH_FOLDER/create-theme.py $NUMBER_OF_COLORS $PWD
+    else
+        echo "Try using: create-theme <number of colors>"
+    fi
 }
