b2d7ca63e5ea932ef81a5658ac42ec56155ace93
Arthur Gonzaga <arthurbilul@gmail.com>
2019-06-24 08:56:39 -0300

    Update to python3

--- command.sh
+++ command.sh
@@ -4,7 +4,7 @@ function create-theme(){
     NUMBER_OF_COLORS=$1
     if [ -n "$NUMBER_OF_COLORS" ]
     then
-        python $PATH_FOLDER/create-theme.py $NUMBER_OF_COLORS $PWD
+        python3 $PATH_FOLDER/create-theme.py $NUMBER_OF_COLORS $PWD
     else
         echo "Try using: create-theme <number of colors>"
     fi
